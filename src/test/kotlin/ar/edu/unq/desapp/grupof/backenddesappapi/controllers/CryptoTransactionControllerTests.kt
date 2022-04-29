a7f706f9131d1288d1be70c79c38569708b8f762
adatri <adatri@unaj.unq.edu.ar>
2022-04-29 15:21:15 -0300

    fix test?

--- src/test/kotlin/ar/edu/unq/desapp/grupof/backenddesappapi/controllers/CryptoTransactionControllerTests.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupof/backenddesappapi/controllers/CryptoTransactionControllerTests.kt
@@ -14,15 +14,15 @@ class CryptoTransactionControllerTests {
 	private val cryptoQuoteController: CryptoTransactionController? = null
 	@Test
 	fun getAllQuotes() {
+		var allTransaction = cryptoQuoteController?.getAll()?.body as ArrayList<*>
+		val allTransactionSizeBefore = allTransaction.size
 		val bnbusdt = CryptoAssetQuoteService().findByCryptoName("BNBUSDT", LocalDateTime.now())
 		val cryptoQuoteToSave = CryptoTransaction("BNBUSDT", 3.0, bnbusdt!!.price.toDouble(), "Aldana Castro")
 		cryptoQuoteController!!.createCryptoQuote(cryptoQuoteToSave)
-		val response = cryptoQuoteController.getAll()
-		val cryptoTransactions = response.body as ArrayList<*>
-		val cryptoTransaction1 = cryptoTransactions[0] as CryptoTransaction
-		assert(cryptoTransaction1.user == "Aldana Castro")
-		assert(cryptoTransaction1.cryptoactive == "BNBUSDT")
-		assert(cryptoTransaction1.id == cryptoQuoteToSave.id)
+
+		allTransaction = cryptoQuoteController?.getAll()?.body as ArrayList<*>
+		val allTransactionSizeAfter = allTransaction.size
+		assert(allTransactionSizeBefore + 1  == allTransactionSizeAfter)
 	}
 
 	@Test
